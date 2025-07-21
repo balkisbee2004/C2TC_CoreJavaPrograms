23d334b84b498d802b14208928df96ccd7e86abe
HP <HP@DESKTOP-FO9L3V5>
2025-07-21 16:26:36 +0530

    Program for entity class using to string method

---
+++ C2TC_CoreJavaPrograms/src/day1/entity/Customer2Demo.java
@@ -0,0 +1,20 @@
+package day1.entity;
+
+public class Customer2Demo {
+	public static void main(String[] args) {
+		Customer2 c1= new Customer2();
+		c1.setCid(101);
+		c1.setName("Balkis");
+		c1.setCity("Puducherry");
+		System.out.println(c1);
+	
+		
+		Customer2 c2= new Customer2();
+		c2.setCid(101);
+		c2.setName("Muffitha");
+		c2.setCity("Chennai");
+		System.out.println(c2);
+		
+
+	}
+}
