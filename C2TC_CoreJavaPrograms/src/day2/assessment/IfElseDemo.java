425de4f4a912b075bff55be36268cf806765e63a
HP <HP@DESKTOP-FO9L3V5>
2025-07-21 21:16:10 +0530

    this is my program for if else statement

---
+++ C2TC_CoreJavaPrograms/src/day2/assessment/IfElseDemo.java
@@ -0,0 +1,20 @@
+package day2.assessment;
+
+public class IfElseDemo {
+	//Program to demonstrate if..else statement
+
+		public static void main(String[] args) {
+			int age=11;
+			
+			if(age>=18)
+				System.out.println(" You are eligible for Voting");	
+			else
+				System.out.println("Sorry! You need to wait more");
+
+			System.out.println("if else demonstration");
+			
+		}
+
+	}
+
+
