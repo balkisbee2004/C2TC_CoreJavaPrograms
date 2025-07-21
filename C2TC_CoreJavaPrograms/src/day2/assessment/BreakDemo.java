0eb382a6cc71443740b3bb34e8c49071137d275b
HP <HP@DESKTOP-FO9L3V5>
2025-07-21 21:02:06 +0530

    this is my break program

---
+++ C2TC_CoreJavaPrograms/src/day2/assessment/BreakDemo.java
@@ -0,0 +1,17 @@
+package day2.assessment;
+
+public class BreakDemo {
+	//Program to demonstrate  break statement
+	
+		public static void main(String[] args) {
+
+			for (int i = 5; i < 10; i++) {
+				if (i == 5)
+					break;
+				System.out.println(i);
+			}
+
+		}
+	}
+
+
