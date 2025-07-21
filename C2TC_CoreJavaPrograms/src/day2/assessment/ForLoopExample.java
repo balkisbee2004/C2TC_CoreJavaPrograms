b6844357a9e5ee2e41c69162ea55599e39e80416
HP <HP@DESKTOP-FO9L3V5>
2025-07-21 21:13:26 +0530

    this is an example program for loop

---
+++ C2TC_CoreJavaPrograms/src/day2/assessment/ForLoopExample.java
@@ -0,0 +1,17 @@
+package day2.assessment;
+
+public class ForLoopExample {
+	//Program to demonstrate for loop
+	
+
+	
+		public static void main(String[] args) 
+		{	
+			for(int i = 1; i<=1000 ; i++)   //(int i = 1000; i<=1;i--)
+			{	
+				System.out.print("Value of i:");
+				System.out.println(i);
+			}
+		}
+	}
+
