775f543fc28d67bf57b1eb77376889814a282825
HP <HP@DESKTOP-FO9L3V5>
2025-07-21 21:24:54 +0530

    Example program for while condition

---
+++ C2TC_CoreJavaPrograms/src/day2/assessment/WhileDemo.java
@@ -0,0 +1,15 @@
+package day2.assessment;
+
+public class WhileDemo {
+	//Program to demonstrate  while do loop
+
+		public static void main(String[] args) 
+		{
+			int i=99;
+			while(i<=100)
+			{
+				System.out.println(i);
+				i++;
+			}
+		}
+	}
