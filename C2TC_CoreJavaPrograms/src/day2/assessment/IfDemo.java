3968c1ff2ed8fbc018c85703b736bba38ddc32c9
HP <HP@DESKTOP-FO9L3V5>
2025-07-21 21:14:47 +0530

    this is my program for if statement

---
+++ C2TC_CoreJavaPrograms/src/day2/assessment/IfDemo.java
@@ -0,0 +1,17 @@
+package day2.assessment;
+
+public class IfDemo {
+	//Program to guess a number in a given range
+	
+
+		public static void main(String[] args) {
+
+			int x = 5;
+			if (x != 5)
+				System.out.println("Value of x is not 5");
+			System.out.println("Value of x is 5");
+		}
+
+	}
+
+
