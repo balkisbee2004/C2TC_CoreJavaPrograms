6bae4706329cefa4d5631750dd726ff1cc44dc6e
HP <HP@DESKTOP-FO9L3V5>
2025-07-21 12:24:17 +0530

    This is my 1st program

--- C2TC_CoreJavaPrograms/src/day1/basics/SampleDemo.java
+++ C2TC_CoreJavaPrograms/src/day1/basics/SampleDemo.java
@@ -4,8 +4,8 @@ package day1.basics;
 public class SampleDemo {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to eclipse!!");
-
+		System.out.println("Welcome to Github!!");
+		System.out.println("My name is Balkis bee");
 	}
 
 }
