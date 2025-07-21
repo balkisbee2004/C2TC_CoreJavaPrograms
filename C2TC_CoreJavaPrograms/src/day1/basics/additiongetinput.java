c805ad0d2aab37d376f9fb7299c202728bd78059
HP <HP@DESKTOP-FO9L3V5>
2025-07-21 14:57:56 +0530

    Addition of 2 integers by getting input in runtime

---
+++ C2TC_CoreJavaPrograms/src/day1/basics/additiongetinput.java
@@ -0,0 +1,15 @@
+package day1.basics;
+import java.util.Scanner;
+
+public class additiongetinput {
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enetr the values");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("Addition of "+a+" and "+b+" is "+c);
+	}
+
+}
