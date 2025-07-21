210129160e128a286002948ec51869585f82bc97
HP <HP@DESKTOP-FO9L3V5>
2025-07-21 14:39:26 +0530

    diffrent methods had given but only the proper main method will run

---
+++ C2TC_CoreJavaPrograms/src/day1/basics/MethodsStatic.java
@@ -0,0 +1,26 @@
+package day1.basics;
+
+public class MethodsStatic 
+{
+	public static void main()
+	{
+		System.out.println("Welcome to main 1");
+		
+	}
+	
+	public static void main(char c[])
+	{
+		System.out.println("Welcome to main 2");
+	}
+	public static void main(String ar)
+	{
+		System.out.println("Welcome to main 3");
+		
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to jfs");
+		System.out.println("Next line");
+	}
+
+}
