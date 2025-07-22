bf12ff493180ba7713080b4b2a2d16b032aa7831
HP <HP@DESKTOP-FO9L3V5>
2025-07-22 13:52:13 +0530

    this is OOPS concept program

---
+++ C2TC_CoreJavaPrograms/src/day3/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,21 @@
+package day3.encapsulation;
+
+public class EncapsulationDemo {
+	//Program to demonstrate class and object 
+
+		public static void main(String[] args) {
+			
+			//object creation 
+			OOPSConceptDemo obj = new OOPSConceptDemo();
+			obj.setSerialNum(101);
+			obj.setName("Pooja");
+			obj.setAge(20);
+			System.out.println(obj);
+			
+		}
+
+	}
+
+	
+
+
