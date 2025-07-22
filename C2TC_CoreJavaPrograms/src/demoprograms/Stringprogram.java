224a60651d5d4a76116cd67e9acc6597831c3867
HP <HP@DESKTOP-FO9L3V5>
2025-07-22 15:16:42 +0530

    this is my string program

---
+++ C2TC_CoreJavaPrograms/src/demoprograms/Stringprogram.java
@@ -0,0 +1,14 @@
+package demoprograms;
+
+import java.util.Scanner;
+
+public class Stringprogram {
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enetr your name: ");
+		String n=input.next();
+		System.out.println("Hello "+n);
+	}
+
+}
