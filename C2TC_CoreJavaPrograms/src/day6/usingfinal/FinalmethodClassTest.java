3c0835212cd6a616b5b0a0e316da0d822a474b0f
HP <HP@DESKTOP-FO9L3V5>
2025-07-25 19:56:48 +0530

    This is my core java program

---
+++ C2TC_CoreJavaPrograms/src/day6/usingfinal/FinalmethodClassTest.java
@@ -0,0 +1,15 @@
+package day6.usingfinal;
+
+public class FinalmethodClassTest extends FinalMethodClass{
+	//Program to demonstrate final method 
+
+		  // Compile time error because we cannot override the final method 
+			/*
+			 * @Override void show() {
+			 * 
+			 * System.out.println("This is the final method of FinalMethodEx class"); }
+			 */
+		   
+	}
+
+
