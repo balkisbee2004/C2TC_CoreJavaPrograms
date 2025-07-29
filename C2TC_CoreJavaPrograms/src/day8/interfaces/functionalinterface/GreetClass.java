b67568d57c73ebfcf7221b5ac2c11bac51b5b972
HP <HP@DESKTOP-FO9L3V5>
2025-07-29 21:17:12 +0530

    This is sample program for Functional interface

---
+++ C2TC_CoreJavaPrograms/src/day8/interfaces/functionalinterface/GreetClass.java
@@ -0,0 +1,14 @@
+package day8.interfaces.functionalinterface;
+
+
+
+	public class GreetClass implements GreetInterface {
+
+		@Override
+		public String greet() {		
+			return "Welcome to the world of Java";
+		}
+
+	
+
+}
